7da3d8beb95a554c3703b3fde7c16cb5475dd24e
taupan14 <taupan.taufik@gmail.com>
2026-07-20 22:50:16 +0700

    fix: adsense code implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,8 +89,7 @@ export default async function RootLayout({
         {/* Google AdSense */}
         <Script
           id="google-adsense"
-          async
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5129841779893087"
           crossOrigin="anonymous"
         />
